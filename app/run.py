caa8b4c5644f87cbbfe34d2b3c8cb3a5c0d19ba6
mariavaghani <maria.vaghani@gmail.com>
2021-04-09 19:25:23 -0400

    removed main function from run

--- app/run.py
+++ app/run.py
@@ -108,11 +108,11 @@ def go():
 
 
 
-def main():
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+#def main():
+    #port = int(os.environ.get("PORT", 5000))
+    #app.run(host='0.0.0.0', port=port, debug=True)
 
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
     
-    main()
+#    main()
