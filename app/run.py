ad06aec6fbdfac5a3879451494141143a644204e
mariavaghani <maria.vaghani@gmail.com>
2021-04-10 11:45:34 -0400

    import fixed

--- app/run.py
+++ app/run.py
@@ -13,8 +13,8 @@ from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
 import joblib
 from sqlalchemy import create_engine
-#from app.message_length_estimator import message_lengths_words, message_length_char
-from message_length_estimator import message_lengths_words, message_length_char
+from app.message_length_estimator import message_lengths_words, message_length_char
+#from message_length_estimator import message_lengths_words, message_length_char
 from sklearn.linear_model import LogisticRegression
 
 
