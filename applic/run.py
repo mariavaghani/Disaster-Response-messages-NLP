6aa3c1886cc40c4b555ed99be524e1b8da69ae5b
Maria Vaghani <54246143+mariavaghani@users.noreply.github.com>
2021-04-10 19:45:47 -0400

    Update run.py

--- applic/run.py
+++ applic/run.py
@@ -64,6 +64,8 @@ print('loaded the pickle now')
 
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
+def hello_world():
+   return 'Hello World’
 @app.route('/index')
 def index():
       
