793965d306df1841be8370c8cd653f032918d00b
Maria Vaghani <54246143+mariavaghani@users.noreply.github.com>
2021-04-10 19:52:50 -0400

    Update __init__.py

--- applic/__init__.py
+++ applic/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from app.message_length_estimator import message_lengths_words, message_length_char
+from applic.message_length_estimator import message_lengths_words, message_length_char
 
 app = Flask(__name__)
 
-from app import run
+from applic import run
