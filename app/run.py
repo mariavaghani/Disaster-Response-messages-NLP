ac3afe1a9baaffd8711fe16f8d9c1286a65ffce6
mariavaghani <maria.vaghani@gmail.com>
2021-04-10 12:06:24 -0400

    modified app.run()

--- app/run.py
+++ app/run.py
@@ -112,8 +112,9 @@ def go():
 
 
 def main():
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    #port = int(os.environ.get("PORT", 5000))
+    #app.run(host='0.0.0.0', port=port, debug=True)
+    app.run()
 
 
 if __name__ == '__main__':
