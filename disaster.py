7adab6252f7394cd2d1746d39f1ce5f8523c3e9c
mariavaghani <maria.vaghani@gmail.com>
2021-04-11 04:34:07 -0400

    templates

--- disaster.py
+++ disaster.py
@@ -1,6 +1,6 @@
 #from .models.message_length_estimator import message_lengths_words, message_length_char
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='applic/templates')
 
 from applic import run
