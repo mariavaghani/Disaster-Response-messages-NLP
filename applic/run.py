e83fc4f286ccafc1286669ec25be3d1eff6877a9
Maria Vaghani <54246143+mariavaghani@users.noreply.github.com>
2021-04-11 00:10:00 -0400

    Update run.py

--- applic/run.py
+++ applic/run.py
@@ -198,14 +198,13 @@ def main():
     else:
         app.run()
 
-print('going to load the app now')
-app.run()
+
 
 if __name__ == '__main__':
     #from applic.message_length_estimator import message_lengths_words, message_length_char
     
     print('going to load the pickle now')
-    """
+    
     # load model
     if local == 0: 
         model = joblib.load("models/classifier.pkl")
@@ -213,5 +212,5 @@ if __name__ == '__main__':
         model = joblib.load("../models/classifier.pkl")
 
     print('loaded the pickle now')
-    """
+    
     main()
