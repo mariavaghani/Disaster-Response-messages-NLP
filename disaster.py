7e84c050b3f0db71bc2e70f2a34c2fdae6da1717
Maria Vaghani <54246143+mariavaghani@users.noreply.github.com>
2021-04-10 21:18:20 -0400

    Update disaster.py

--- disaster.py
+++ disaster.py
@@ -1,2 +1,2 @@
-from applic.message_length_estimator import message_lengths_words, message_length_char
+#from applic.message_length_estimator import message_lengths_words, message_length_char
 from applic import run
